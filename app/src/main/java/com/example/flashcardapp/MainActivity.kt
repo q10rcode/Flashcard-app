73a27f347b6161cb605f4910c5e771da336d875e
q10rcode <90580184+q10rcode@users.noreply.github.com>
2022-09-19 21:54:32 -0700

    Finished the optional tasks for the project!

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.flashcardapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +15,59 @@ class MainActivity : AppCompatActivity() {
         val cardQuestion = findViewById<TextView>(R.id.card_question)
         val cardAnswer = findViewById<TextView>(R.id.card_answer)
 
-        cardQuestion.setOnClickListener{
+        //Switch from Question to Answer
+        cardQuestion.setOnClickListener {
             cardQuestion.visibility = View.INVISIBLE
             cardAnswer.visibility = View.VISIBLE
         }
+
+        //Switch from Answer to Question
+        cardAnswer.setOnClickListener {
+            cardQuestion.visibility = View.VISIBLE
+            cardAnswer.visibility = View.INVISIBLE
+
+
+        }
+        val firstOp = findViewById<TextView>(R.id.wrong1)
+        val secondOp = findViewById<TextView>(R.id.wrong2)
+        val thirdOp = findViewById<TextView>(R.id.correct)
+
+        firstOp.setOnClickListener {
+            firstOp.setBackgroundColor(Color.rgb(255, 0, 0))
+        }
+
+        secondOp.setOnClickListener {
+            secondOp.setBackgroundColor(Color.rgb(255, 0, 0))
+        }
+
+        thirdOp.setOnClickListener {
+            thirdOp.setBackgroundColor(Color.rgb(0, 255, 0))
+        }
+
+
+        var isShowing = true
+        val hideButton = findViewById<ImageView>(R.id.toggle_choices_visibility)
+
+        //make choice appear and disappear
+        hideButton.setOnClickListener {
+            if (isShowing){
+                hideButton.setImageResource(R.drawable.hidden)
+
+                firstOp.visibility = View.INVISIBLE
+                secondOp.visibility = View.INVISIBLE
+                thirdOp.visibility = View.INVISIBLE
+
+                isShowing = false
+            } else {
+                hideButton.setImageResource(R.drawable.eye)
+
+                firstOp.visibility = View.VISIBLE
+                secondOp.visibility = View.VISIBLE
+                thirdOp.visibility = View.VISIBLE
+
+                isShowing = true
+            }
+        }
+
     }
 }
