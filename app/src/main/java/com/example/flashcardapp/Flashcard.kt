fbbca4e2e9943ccc37a3acf98cad86950ef4c75c
q10rcode <90580184+q10rcode@users.noreply.github.com>
2022-10-10 18:17:08 -0700

    Finished Lab 3 Requirements!

---
+++ app/src/main/java/com/example/flashcardapp/Flashcard.kt
@@ -0,0 +1,16 @@
+package com.example.flashcardapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Flashcard(
+    @ColumnInfo(name = "question") var question: String,
+    @ColumnInfo(name = "answer") var answer: String,
+    @ColumnInfo(name = "wrong_answer_1") val wrongAnswer1: String? = null,
+    @ColumnInfo(name = "wrong_answer_2") val wrongAnswer2: String? = null,
+    @PrimaryKey(autoGenerate = true) var uuid: Int = 0,
+)
